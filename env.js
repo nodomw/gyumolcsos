b57b89ba9f71d0c6da1ef4c2f1e77f8ad81083c8
Notheisz Mátyás <nodomatyi@gmail.com>
2025-11-14 17:54:30 +0100

    initial backend with validation

---
+++ env.js
@@ -0,0 +1,14 @@
+export default {
+  server: {
+    port: 3000
+  },
+  db: {
+    host: "127.0.0.1",
+    user: "root",
+    password: "newnew",
+    database: "gyumolcs",
+    connectionTimeout: 15,
+    connectionLimit: 10,
+    queueLimit: 15
+  }
+}
---
+++ index.js
@@ -0,0 +1,106 @@
+import config from "./env.js";
+import { createPool } from "mariadb";
+import express from "express";
+
+const app = express();
+const pool = createPool(config.db);
+
+app.use(express.json());
+
+app.get("/fruits", (req, res) => {
+  pool
+    .execute("select * from fruits")
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Database query failed");
+    });
+});
+
+app.get("/fruits/:id", (req, res) => {
+  let id = req.params.id;
+  if (typeof id === "undefined" || id <= 1) {
+    res.status(400).redirect("https://http.cat/400");
+  } else {
+    pool
+      .execute("select * from fruits where id = ?", [id])
+      .then((result) => {
+        if (result.length === 0) {
+          res.status(404).redirect("https://http.cat/404");
+        } else {
+          res.status(200).json(result);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Database query failed");
+      });
+  }
+});
+
+app.post("/fruits", (req, res) => {
+  if (typeof req.body == "undefined") {
+    res.status(400).redirect("https://http.cat/400");
+  }
+
+  let name = req.body.name;
+  let price = req.body.price;
+
+  if (
+    typeof name == "undefined" ||
+    typeof price == "undefined" ||
+    isNaN(price) ||
+    !(price > 0)
+  ) {
+    res.status(400).redirect("https://http.cat/400");
+  } else {
+    pool
+      .execute("insert into fruits (name, price) values (?, ?)", [name, price])
+      .then((result) => {
+        res.status(201).json(result);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Database insert failed");
+      });
+  }
+});
+
+app.put("/fruits/:id", (req, res) => {
+  let id = req.params.id;
+  let name = req.body.name;
+  let price = req.body.price;
+  if (
+    isNaN(id) ||
+    id <= 1 ||
+    typeof name == "undefined" ||
+    typeof price == "undefined" ||
+    isNaN(price) ||
+    !(price > 0)
+  ) {
+    res.status(400).redirect("https://http.cat/400");
+  } else {
+    conn
+      .execute("update fruits set name = ?, price = ? where id = ?", [
+        name,
+        price,
+      ])
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Database update failed");
+      });
+  }
+});
+
+app.listen(config.server.port, (err) => {
+  if (err) {
+    console.warn(err);
+  }
+
+  console.log(`running on ${config.server.port} yurrrrr`);
+});
